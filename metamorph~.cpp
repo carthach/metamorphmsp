1069721e09a1dbbe01ba1b750faa2eb00e0fbc13
Cárthach Ó Nuanáin <carthach@carthach.tk>
2017-05-19 16:25:13 +0100

    More simplications and class rename

--- metamorph~.cpp
+++ metamorph~.cpp
@@ -6,32 +6,31 @@
 
 using namespace metamorph;
 
-class Example : public MspCpp6<Example> {
+class Metamorph : public MspCpp6<Metamorph> {
 public:
     FX metamorph;
     
-    Example(t_symbol * sym, long ac, t_atom * av) {
+    Metamorph(t_symbol * sym, long ac, t_atom * av) {
         setupIO(3, 1);
         post("object created");
-        
         
         metamorph.preserve_transients(true);
         metamorph.preserve_envelope(true);
     }
     
-    ~Example() {
+    ~Metamorph() {
         post("object freed");
         
         
     }
     
     // methods:
-    void bang(long inlet) {
-        post("bang in inlet %i!", inlet);
-    }
-    void test(long inlet, t_symbol * s, long ac, t_atom * av) {
-        post("%s in inlet %i (%i args)", s->s_name, inlet, ac);
-    }
+//    void bang(long inlet) {
+//        post("bang in inlet %i!", inlet);
+//    }
+//    void test(long inlet, t_symbol * s, long ac, t_atom * av) {
+//        post("%s in inlet %i (%i args)", s->s_name, inlet, ac);
+//    }
     
     void handleFloat(long inlet, double v) {
         std::string param = "";
@@ -58,45 +57,32 @@ public:
     
     // default signal processing method is called 'perform'
     void perform(double **ins, long numins, double **outs, long numouts, long sampleframes) {
-        //		// example code to invert inputs
-        std::vector<sample> inBuffer(sampleframes);
-        std::vector<sample> outBuffer(sampleframes);
-        
         double* in = ins[0];
         double* out = outs[0];
         
-        for(int i=0; i<sampleframes; i++)
-        {
-            inBuffer[i] = in[i];
-        }
-        
-        metamorph.process_frame(sampleframes, &inBuffer[0], sampleframes, &outBuffer[0]);
-        
-        for(int i=0; i<sampleframes; i++)
-        {
-            out[i] = outBuffer[i];
-        }
+        metamorph.process_frame(sampleframes, in, sampleframes, out);
     }
     
     //	// optional method: gets called when the dsp chain is modified
-    //	// if not given, the MspCpp will use Example::perform by default
+    //	// if not given, the MspCpp will use Metamorph::perform by default
     void dsp(t_object * dsp64, short *count, double samplerate, long maxvectorsize, long flags) {
         // specify a perform method manually:
         metamorph.reset();
         
+        metamorph.hop_size(maxvectorsize);
+        metamorph.frame_size(maxvectorsize*4);
         
-        
-        REGISTER_PERFORM(Example, perform);
+        REGISTER_PERFORM(Metamorph, perform);
     }
 };
 
 C74_EXPORT int main(void) {
     // create a class with the given name:
-    Example::makeMaxClass("metamorph~");
-    REGISTER_METHOD(Example, bang);
-    REGISTER_METHOD_GIMME(Example, test);
+    Metamorph::makeMaxClass("metamorph~");
+//    REGISTER_METHOD(Metamorph, bang);
+//    REGISTER_METHOD_GIMME(Metamorph, test);
     
-    REGISTER_INLET_FLOAT(Example, handleFloat);
-    REGISTER_INLET_FLOAT(Example, handleFloat);
-    REGISTER_INLET_FLOAT(Example, handleFloat);
+    REGISTER_INLET_FLOAT(Metamorph, handleFloat);
+    REGISTER_INLET_FLOAT(Metamorph, handleFloat);
+    REGISTER_INLET_FLOAT(Metamorph, handleFloat);
 }
